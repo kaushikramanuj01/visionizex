f0d9eb736fa4d889ae69604441ab1c65d6a4740a
kaushik ramanuj <kaushikramanuj01@gmail.com>
2024-01-29 02:48:35 +0530

    nav responsive

--- navbar.php
+++ navbar.php
@@ -7,11 +7,11 @@
         <!-- <a href="javascript:void(0)" onclick="showImageGeneration()">Generate</a>
             <a href="javascript:void(0)" onclick="showGallery()">Explore ideas</a> -->
 
-        <a href="index.php" id="imageButton">Generate</a>
-        <a href="creation.php" id="chatButton">Explore ideas</a>
-        <a href="blog.php" id="chatButton">Blog</a>
-        <a href="contact.php" id="chatButton">Contact</a>
-        <a href="profile.php" id="chatButton">Profile</a>
+        <a href="index.php" id="imageButton" class="nav_hide2">Generate</a>
+        <a href="creation.php" id="chatButton" class="nav_hide2">Explore ideas</a>
+        <a href="blog.php" id="chatButton" class="nav_hide">Blog</a>
+        <a href="contact.php" id="chatButton" class="nav_hide">Contact</a>
+        <a href="profile.php" id="chatButton" class="nav_hide">Profile</a>
         <!-- <button id="imageButton">Image</button> -->
         <!-- <button id="chatButton">Chat</button> -->
     </div>
@@ -28,7 +28,7 @@
             $usercredit = $SubDB->countcredit();
             ?>
         <div class="credit">
-            <div>
+            <div class="f-center">
                 <span class="credit_img"> <img src="images/logo/coin.png" alt="" srcset=""></span>
                 <span id="credit_count">
                     <?php echo $usercredit; ?>
@@ -39,25 +39,42 @@
             <?php }  ?>
 
         <?php if(isset($_SESSION['login']) && $_SESSION['login'] == 1){ ?>
-        <a href="logout.php" id="logoutbutton">Logout</a>
+        <a href="logout.php" id="logoutbutton" class="nav_hide">Logout</a>
         <?php }else{ ?>
-        <a href="login.php" id="loginbutton">Login</a>
+        <a href="login.php" id="loginbutton" class="nav_hide">Login</a>
         <?php } ?>
         <div class="i-block">
             <!-- <div class="premium_btn_div"> -->
-                <a href="premium.php" class="premium_btn_div" id="premium_btn">Premium</a>
+                <a href="premium.php" class="premium_btn_div nav_hide" id="premium_btn">Premium</a>
             <!-- </div> -->
         </div>
-        <!-- <button id="openSidebar" onclick="w3_toggle()">☰</button> -->
+        <button id="openSidebar" onclick="w3_toggle()">☰</button>
         <!-- <button class="rk_btn w3-button w3-xlarge" onclick="w3_toggle()">button</button> -->
     </div>
 </div>
 
+<div class="nav-second">
+    <a href="index.php" class="border-b">Generate</a>
+    <a href="creation.php">Explore ideas</a>
+</div>
+
 <div id="mySidebar">
   <a href="#" class="w3-bar-item w3-button" onclick="w3_close()">Close &times;</a>
+  <!-- <div class="i-block"> -->
+    <!-- <div class="premium_btn_div"> -->
+        <a href="premium.php" class="premium_btn_div w3-bar-item w3-button" id="premium_btn">Premium</a>
+    <!-- </div> -->
+  <!-- </div> -->
+  <a href="blog.php" class="w3-bar-item w3-button">Blog</a>
   <a href="contact.php" class="w3-bar-item w3-button">Contact</a>
-  <a href="blog.php" class="w3-bar-item w3-button">Blogs</a>
-  <a href="#" class="w3-bar-item w3-button">Link 3</a>
+  <a href="profile.php" class="w3-bar-item w3-button">Profile</a>
+
+  <?php if(isset($_SESSION['login']) && $_SESSION['login'] == 1){ ?>
+    <a href="logout.php" class="w3-bar-item w3-button" id="logoutbutton">Logout</a>
+    <?php }else{ ?>
+    <a href="login.php" class="w3-bar-item w3-button" id="loginbutton">Login</a>
+  <?php } ?>
+
 </div>
 
 
