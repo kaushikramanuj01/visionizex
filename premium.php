fcc5199ac6f950ca2710a26306cd3f5e18efde51
kaushik ramanuj <kaushikramanuj01@gmail.com>
2024-01-30 03:44:44 +0530

    premium page responsive

--- premium.php
+++ premium.php
@@ -158,6 +158,73 @@ include 'navbar.php';
         margin-top: 20px;
         width: 91%;
         margin-bottom: 15px;
+    }    
+    .title-s{
+        margin-top: 3px;
+        font-size: 16px;
+    }
+    .rv-div{
+        display: flex;
+        justify-content: center;
+    }
+
+    @media screen and (max-width: 1024px) {
+        .sub_cart {
+            width: 88%;
+        }
+    }
+    @media screen and (max-width: 770px) {
+        .sub_cart {
+            width: 92%;
+        }
+        .sub_cart{
+            width: 95%;
+        }
+        .pack_box{
+            width: 32%;
+            margin: 0px 4px;
+        }
+        .pack_box h5{
+            font-size: 19px;
+            margin-top: 14px;
+        }
+        .buy_btn{
+            padding: 4px;
+            font-size: 14px;            
+        }
+        .info_box {
+            font-size: 14px;
+        }
+    }
+    @media screen and (max-width: 480px) {
+        .rv-div {
+            flex-wrap: wrap;
+        }
+        .pack_box {
+            width: 46%;
+        }
+        .mt14{
+            margin-top: 14px;
+        }
+    }
+    @media screen and (max-width: 390px) {
+        .sub_cart{
+            padding: 3px 0px;
+        }
+        .pack_box {
+            width: 85%;
+            margin-top: 10px;
+        }
+        .pack_box h5 {
+            font-size: x-large;
+        }
+        .title-s {
+            font-size: 18px;
+        }
+        .buy_btn {
+            padding: 6px;
+            font-size: 17px;
+        }
     }
     </style>
 </head>
@@ -166,13 +233,12 @@ include 'navbar.php';
 
     <div class="main_cart">
         <div class="sub_cart">
-        <div style="display: flex;">
+        <div class="rv-div"> 
             <div class="pack_box b_g1">
                 <div class="background_color b_g1"></div>
                 <h5>Basic</h5>
                 <!-- <div class="current">(Current)</div> -->
-                <div style="margin-top: 3px;">
-                    <!-- USD $0/month -->
+                <div class="title-s">
                     USD $0
                 </div>
                 <span>
@@ -195,7 +261,7 @@ include 'navbar.php';
 
                 <h5>Classic</h5>
                 <!-- <div class="current">(Current)</div> -->
-                <div style="margin-top: 3px;">
+                <div class="title-s">
                     Rs. 1500 / 2000 Credit Points
                 </div>
                 <span>
@@ -213,12 +279,12 @@ include 'navbar.php';
                 </ul>
                 </div>
             </div>
-            <div class="pack_box b_g3">
+            <div class="pack_box b_g3 mt14">
                 <div class="background_color b_g3"></div>
 
                 <h5>Expert</h5>
                 <!-- <div class="current">(Current)</div> -->
-                <div style="margin-top: 3px;">
+                <div class="title-s">
                     USD 2000 / 4000 Credit Points
                 </div>
                 <span>
@@ -241,39 +307,8 @@ include 'navbar.php';
             </div>
         </div>
     </div>
-    <!-- <table>
-        <thead>
-            <tr>
-                <th>Package</th>
-                <th>Price</th>
-                <th>Credit</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Basic Package</td>
-                <td>Rs.1000</td>
-                <td>2000</td>
-                <td> <a href="buy.php?pack=1" class="buy-button">Buy</a></td>
-            </tr>
-            <tr>
-                <td>Standard Package</td>
-                <td>Rs.1500</td>
-                <td>4000</td>
-                <td><a href="buy.php?pack=2" class="buy-button">Buy</a></td>
-            </tr>
-            <tr>
-                <td>Premium Package</td>
-                <td>Rs.2000</td>
-                <td>6000</td>
-                <td><a href="buy.php?pack=3" class="buy-button">Buy</a></td>
-            </tr>
-        </tbody>
-    </table> -->
 
     <script src="main.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </body>
-
 </html>
